3b68a08f5830d40b54783cfcbe3a40ffa06a77ca
belalfawzy <belalfawzy123@gmail.com>
2025-04-17 17:02:44 +0200

    first commit

---
+++ Assignment18_EFcore/AppDbContext.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment18_EFcore
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<School> school;
+        public DbSet<Department> departments;
+        public DbSet<Teacher> teachers;
+        public DbSet<TeacherTransfer> teachersTransfer;
+        protected override void OnConfiguring(DbContextOptionsBuilder opt) 
+            => opt.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=initialDB;Integrated Security=True;");
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<School>()
+                .HasOne(s => s.dept)
+                .WithMany(d => d.Schools)
+                .HasForeignKey(s => s.deptId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Teacher>()
+                .HasOne(t => t.School)
+                .WithMany(s => s.Teachers)
+                .HasForeignKey(t => t.SchoolId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<TeacherTransfer>()
+                .HasOne(tt => tt.Teacher)
+                .WithMany(t => t.TeacherTransfers)
+                .HasForeignKey(tt => tt.TeacherID)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<TeacherTransfer>()
+                .HasOne(tt => tt.FromSchool)
+                .WithMany(s => s.FromTransfers)
+                .HasForeignKey(tt => tt.FromSchoolID)
+                .OnDelete(DeleteBehavior.Restrict); 
+
+            modelBuilder.Entity<TeacherTransfer>()
+                .HasOne(tt => tt.ToSchool)
+                .WithMany(s => s.ToTransfers)
+                .HasForeignKey(tt => tt.ToSchoolID)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
---
+++ Assignment18_EFcore/Program.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment18_EFcore
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+       
+    }
+
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public List<School> Schools { get; set; }
+    }
+
+    public class School
+    {
+        public int Id { get; set; }
+        public int deptId { get; set; }
+        public string name { get; set; }
+        public string type { get; set; }
+
+        public Department dept { get; set; }
+        public List<Teacher> Teachers { get; set; }
+        public List<TeacherTransfer> FromTransfers { get; set; }
+        public List<TeacherTransfer> ToTransfers { get; set; }  
+    }
+
+
+    public class Teacher
+    {
+        public int Id { get; set; }
+        public int SchoolId { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public int NationalID { get; set; }
+        public string Code { get; set; }
+        public string Job { get; set; }
+        public string Phone { get; set; }
+        public string Qualification { get; set; }
+        public DateTime QualificationDate { get; set; }
+        public DateTime HiringDate { get; set; }
+        public string Address { get; set; }
+        public string Notes { get; set; }
+
+        public School School { get; set; }
+        public List<TeacherTransfer> TeacherTransfers { get; set; }
+    }
+
+    public class TeacherTransfer
+    {
+        public int Id { get; set; }
+        public int TeacherID { get; set; }
+        public int FromSchoolID { get; set; }
+        public int ToSchoolID { get; set; }
+        public DateTime Date { get; set; }
+
+        public Teacher Teacher { get; set; }
+        public School FromSchool { get; set; }
+        public School ToSchool { get; set; }
+    }
+
+}
